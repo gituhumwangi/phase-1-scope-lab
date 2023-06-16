bfb0de185d3ee7610d1f2d82dc70836f2456f5a6
dennis <dennis.mwangi@student.moringaschool.com>
2023-06-16 17:34:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "Tate"
+function upperCaseCustomerName () {
+    console.log(customerName.toUpperCase())
+}
+
+function setBestCustomer () {
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer () {
+    window.bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "Andrew"
+}
